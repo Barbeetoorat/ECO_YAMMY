8360178b6ac934560483fdc96d41dbe2e3618ebd
Barbeetoorat <reversesx@gmail.com>
2017-03-03 04:39:43 +0700

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,82 +1,85 @@
-function ajax() { Ajax отправка формы
-	var msg = $(#form).serialize();
+function ajax() { //Ajax отправка формы
+$("#loader").fadeIn();
+	var msg = $("#form").serialize();
 	$.ajax({
-		type POST,
-		url .send.php,
-		data msg,
-		success function(data) {
-			$(#results).html(data);
+		type: "POST",
+		url: "./send.php",
+		data: msg,
+		success: function(data) {
+			$("#result").html(data);
+			$("#loader").fadeOut();
 		},
-		error  function(xhr, str){
-			alert(Возникла ошибка!);
+		error:  function(xhr, str){
+		$("#loader").fadeOut();
+			alert("Возникла ошибка!");
 		}
 	});
 }
 
-jQuery.fn.notExists = function() { Проверка на существование элемента
+jQuery.fn.notExists = function() { //Проверка на существование элемента
 	return $(this).length == 0;
 }
 
-$(document).ready(function(){ Валидация формы
-	$(.send).validation(
-		$(.name).validate({
-			test blank letters, 
-			invalid function(){
-				if($(this).nextAll(.error).notExists()) {
-					$(this).after('div class=errorВведите корректное имяdiv');
-					$(this).nextAll(.error).delay(2000).fadeOut(slow);
+$(document).ready(function(){ //Валидация формы
+	$(".send").validation(
+		$(".name").validate({
+			test: "blank letters", 
+			invalid: function(){
+				if($(this).nextAll(".error").notExists()) {
+					$(this).after('<div class="error">Введите корректное имя</div>');
+					$(this).nextAll(".error").delay(2000).fadeOut("slow");
 					setTimeout(function () {
-						$(.name).next(.error).remove();
+						$(".name").next(".error").remove();
 					}, 2600);
 				}
 			},
-			valid function(){
-				$(this).nextAll(.error).remove();
+			valid: function(){
+				$(this).nextAll(".error").remove();
 			}
 		}),
-		$(.email).validate({
-			test blank email,
-			invalid function(){
-				if($(this).nextAll(.error).notExists()) {
-					$(this).after('div class=errorВведите корректный emaildiv');
-					$(this).nextAll(.error).delay(2000).fadeOut(slow);
+		$(".email").validate({
+			test: "blank email",
+			invalid: function(){
+				if($(this).nextAll(".error").notExists()) {
+					$(this).after('<div class="error">Введите корректный email</div>');
+					$(this).nextAll(".error").delay(2000).fadeOut("slow");
 					setTimeout(function () {
-						$(.email).next(.error).remove();
+						$(".email").next(".error").remove();
 					}, 2600);
 				}
 			},
-			valid function(){
-				$(this).nextAll(.error).remove();
+			valid: function(){
+				$(this).nextAll(".error").remove();
 			}
 		}),
-		$(.subject).validate({
-			test blank, 
-			invalid function(){
-				if($(this).nextAll(.error).notExists()) {
-					$(this).after('div class=errorВведите темуdiv');
-					$(this).nextAll(.error).delay(2000).fadeOut(slow);
+		$(".subject").validate({
+			test: "blank", 
+			invalid: function(){
+				if($(this).nextAll(".error").notExists()) {
+					$(this).after('<div class="error">Введите тему</div>');
+					$(this).nextAll(".error").delay(2000).fadeOut("slow");
 					setTimeout(function () {
-						$(.subject).next(.error).remove();
+						$(".subject").next(".error").remove();
 					}, 2600);
 				}
 			},
-			valid function(){
-				$(this).nextAll(.error).remove();
+			valid: function(){
+				$(this).nextAll(".error").remove();
 			}
 		}),
-		$(.message).validate({
-			test blank, 
-			invalid function(){
-				if($(this).nextAll(.error).notExists()) {
-					$(this).after('div class=errorВведите сообщениеdiv');
-					$(this).nextAll(.error).delay(2000).fadeOut(slow);
+		$(".message").validate({
+			test: "blank", 
+			invalid: function(){
+				if($(this).nextAll(".error").notExists()) {
+					$(this).after('<div class="error">Введите сообщение</div>');
+					$(this).nextAll(".error").delay(2000).fadeOut("slow");
 					setTimeout(function () {
-						$(.message).next(.error).remove();
+						$(".message").next(".error").remove();
 					}, 2600);
 				}
 			},
-			valid function(){
-				$(this).nextAll(.error).remove();
+			valid: function(){
+				$(this).nextAll(".error").remove();
 			}
 		})
 	);
